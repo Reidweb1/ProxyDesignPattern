79ce3976970d9d5eb8077602ce0a4e4cd9d8209a
Reid Weber <reid@meshstudio.io>
2017-05-04 09:47:09 -0700

    Initial Commit

---
+++ ProxyDesignPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProxyDesignPattern
+//
+//  Created by Reid Weber on 5/4/17.
+//  Copyright © 2017 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProxyDesignPattern/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProxyDesignPattern
+//
+//  Created by Reid Weber on 5/4/17.
+//  Copyright © 2017 Reid Weber. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
